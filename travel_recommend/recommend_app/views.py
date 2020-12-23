506b78a4a035b4dc2b0a781c588a1cfa6b451120
bonghwan <lbh6314@naver.com>
2020-12-24 00:52:13 +0900

    add_SVD

--- travel_recommend/recommend_app/views.py
+++ travel_recommend/recommend_app/views.py
@@ -1,92 +1,80 @@
 from django.shortcuts import render
 
-# Create your views here.
+#!pip install scikit-surprise
+
 import pandas as pd # raw dataset
+from surprise import SVD, accuracy # SVD model, 평가
+from surprise import Reader, Dataset # SVD model의 dataset
+import surprise
 
-# CNN
-import numpy as np
-import matplotlib.pyplot as plt
+filepath = 'C:\work\Data_teampro\internal/placerating.csv'
+results =[]
 
-from keras.datasets import imdb
-from keras.preprocessing.sequence import pad_sequences
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import OneHotEncoder, StandardScaler
+def Cal_Svd(filepath, user_id):
+    # 1. raw dataset
+    rating = pd.read_csv(filepath)
+    rating.head()   #   critic(user)   title(item)   rating
+    
+    
+    rating['userId'].value_counts()
+    rating['placeId'].value_counts()
+    
+    # 관광 vs 미관광
+    tab = pd.crosstab(rating['userId'], rating['placeId'])
+    #print(tab)
+    
+    # rating
+    # 두 개의 집단변수를 가지고 나머지 rating을 그룹화
+    rating_g = rating.groupby(['userId', 'placeId'])
+    rating_g.sum()
+    tab = rating_g.sum().unstack() # 행렬구조로 변환
+    #print(tab)
+    #print(tab.info())
+    #사용자 2이 가지 않은 곳, 1,15, 39....
+    
+    # 2. rating 데이터셋 생성
+    reader = Reader(rating_scale= (1, 5)) # 평점 범위
+    data = Dataset.load_from_df(df=rating, reader=reader)
+    # rating이라는 데이터프레임은 reader(1~5)의 평점 범위를 가진다.
+    #print(data)
+    
+    # 3. train/test set
+    train = data.build_full_trainset() # 훈련셋
+    test = train.build_testset() # 검정셋
+    
+    # 4. model 생성
+    #help(SVD)
+    model = SVD(n_factors=100, n_epochs=20, random_state=123)
+    model.fit(train) # model 생성
+    
 
+    
+    # 5. user_id 입력
+    user_id = 1 # 추천대상자
+    item_ids = range(0, 2106) # placeId 범위
+    actual_rating = 0 # 평점
+    
+    predict_result = []
+    
+    for item_id in item_ids :
+      #print(model.predict(user_id, item_id, actual_rating))
+      if not actual_rating in tab:
+        actual_rating = 0
+      #print(model.predict(user_id, item_id, trating))
+      predict_result.append(model.predict(user_id, item_id, actual_rating))
+    
+    ddff = pd.DataFrame(predict_result)
+    print(ddff)
+    # max(ddff.est.values)
+    # ddff.est.argmax()
+    # ddff.loc[910]
+    # 유저 1 추천 여행지 상위 5개
+    result = ddff.sort_values(by='est', ascending=False)[:5]
+    #print(result)
+    results.append(result)
 
-# 1. raw dataset
-num_features = 3000
-sequence_length = 2000
-embedding_dimension = 1000
-
-rating = pd.read_csv("C:\work\Data_teampro\internal/placerating.csv")
-rating.head()   #   critic(user)   title(item)   rating
-
-# x, y 정의
-x = rating['userId']
-y = rating['rating']
-
-print(len(x))
-print(len(y))
-"""
-######## 스케일링 안하면 loss가 매우매우 작게 나옴 (음수...엄청난 자릿수로)
-
-def vectorize_sequences(sequences, dimension=10000):
-    # 크기가 (len(sequences), dimension)이고 모든 원소가 0인 행렬을 만듭니다.
-    results = np.zeros((len(sequences), dimension))
-    for i, sequence in enumerate(sequences):
-        results[i, sequence] = 1. # results[i]에서 특정 인덱스의 위치를 1로 만듭니다.
-    return results
-
-x_train = vectorize_sequences(train_data) # 훈련 데이터를 벡터로 변환합니다.
-x_test = vectorize_sequences(test_data) # 테스트 데이터를 벡터로 변환합니다.
-
-
-# label 원 핫 인코딩
-onehot = OneHotEncoder(categories = 'auto')
-# print(onehot)
-y = onehot.fit_transform(y[:,np.newaxis]).toarray()
-# print(y[:5], y.shape)
-
-#train_labels = np.reshape(train_labels, (-1, NUM_CLASSES)) you're changing the shape to [10, 5].
-
-"""
-# # # feature 표준화
-scaler = StandardScaler()
-x = np.asarray(x).astype('float32').reshape((-1,1))
-#y = np.asarray(y).astype('float32').reshape((-1,1))
-x_scale = scaler.fit_transform(x)
-
-# y_scale = scaler.fit_transform(y)
-
-# train / test
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3, random_state = 1)
-
-
-##### 표준화 후 차원 맞는 지 확인 필요.
-# 요기부분
-x_trains = []
-for i in x_train:
-  x_trains.append(x_train)
-
-x_tests = []
-for i in x_test:
-  x_tests.append(x_test)
-
-
-x_train = [x_train]
-x_test = [x_test]
-print(type(x_train))
-
-x_train = pad_sequences(x_trains, maxlen = sequence_length)
-x_test = pad_sequences(x_tests, maxlen = sequence_length)
-
-
-def vectorize_sequences(sequences, dimension=10000):
-    # 크기가 (len(sequences), dimension)이고 모든 원소가 0인 행렬을 만듭니다.
-    results = np.zeros((len(sequences), dimension))
-    for i, sequence in enumerate(sequences):
-        results[i, sequence] = 1. # results[i]에서 특정 인덱스의 위치를 1로 만듭니다.
-    return results
-
-x_train = vectorize_sequences(x_train) # 훈련 데이터를 벡터로 변환합니다.
-x_test = vectorize_sequences(x_test) # 테스트 데이터를 벡터로 변환합니다.
+if __name__ == '__main__':
+    Cal_Svd(filepath, 1)
+    print(results[0])
+    print(type(results[0]))     
+    print(results[0]['iid'])        # placeId
